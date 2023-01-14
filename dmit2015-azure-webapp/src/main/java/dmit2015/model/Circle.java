8387d4604e6252cf2e7fa35a48fba104bf7c1b6c
James Skrlj <jskrlj1@nait.ca>
2023-01-13 20:50:14 -0700

    Class 3 - C# -> Java Circle Example

---
+++ dmit2015-azure-webapp/src/main/java/dmit2015/model/Circle.java
@@ -0,0 +1,67 @@
+package dmit2015.model;
+
+public class Circle {
+    private double radius;
+
+    public double getRadius() {
+        return radius;
+    }
+
+    // Check Exception
+    public void setRadius(double radius) throws Exception {
+        if (radius <= 0)
+        {
+            throw new Exception("Radius must be a positive non-zero number.");
+        }
+        this.radius = radius;
+    }
+
+    // Runtime Exception
+//    public void setRadius(double radius) {
+//        if (radius <= 0)
+//        {
+//            throw new RuntimeException("Radius must be a positive non-zero number.");
+//        }
+//        this.radius = radius;
+//    }
+
+    public Circle() {
+        this.radius = 1;
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double Area() {
+        return Math.PI * radius * radius;
+    }
+
+    public double Perimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    public static void main(String[] args) {
+
+        Circle circle1 = new Circle();
+        System.out.printf("The radius of circle1 is %s \n", circle1.getRadius());
+        System.out.printf("The area of circle1 is %.5f \n", circle1.Area());
+        System.out.printf("The perimeter of circle1 %.2f \n", circle1.Perimeter());
+
+        try
+        {
+            circle1.setRadius(5);
+
+            System.out.println("The radius of circle1 is " + circle1.getRadius());
+            System.out.printf("The area of circle1 is %.5f \n", circle1.Area());
+            System.out.printf("The perimeter of circle1 %.5f \n", circle1.Perimeter());
+
+            circle1.setRadius(-25);
+            System.out.println("A exception should have been thrown");
+        }
+        catch (Exception ex)
+        {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
